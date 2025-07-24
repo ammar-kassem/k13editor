0dababb9c64080ee58f709c1bd65c6f57d0cbdab
Ammar Kassem <ammarkasswm666@gmail.com>
2025-07-24 12:55:53 +0300

    Update settings.py
    .

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,13 +119,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Media files (uploaded content)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# Build paths inside the 
 
+# WhiteNoise for serving static files in production
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # يجب أن تكون قبل Django's CommonMiddleware
+    # ...
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
