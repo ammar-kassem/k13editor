b59d181646ca9a0724843bc79c7a91d85cce5059
Ammar Kassem <ammarkasswm666@gmail.com>
2025-07-24 13:43:22 +0300

    Update urls.py
    .

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,11 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projects.urls')),  # ⬅️ هذا السطر مهم جداً
+    path('', include('projects.urls')),  # غيّر projects إذا كان اسم تطبيقك مختلف
 ]
 
+# ✅ دعم عرض ملفات media
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # هذا السطر يجعل الملفات تظهر حتى عندما DEBUG=False
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
