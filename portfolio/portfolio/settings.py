6274ee4d1e297b92e9332381bdb48d0f7c587f20
Ammar Kassem <ammarkasswm666@gmail.com>
2025-07-24 10:32:29 +0300

    Update settings.py
    .

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
+
 
 
 
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
