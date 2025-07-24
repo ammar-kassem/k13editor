66e86e44e995ba97013eb704c74c0d6076beda76
Ammar Kassem <ammarkasswm666@gmail.com>
2025-07-24 10:25:45 +0300

    Update settings.py
    .

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -78,7 +78,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
